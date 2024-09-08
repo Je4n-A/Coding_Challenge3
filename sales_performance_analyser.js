22b9f49b3cd5791a73d8c51fdeaa0da22af23c0e
Je4n-A <jeanma6742@gmail.com>
2024-09-07 21:49:38 -0400

    "Create determinePerformanceRating function"

--- sales_performance_analyser.js
+++ sales_performance_analyser.js
@@ -5,11 +5,32 @@ function CalculateAverageSales(salesfigures) {
 
 let Sum_sales = salesfigures.reduce((acc,curr) => acc + curr, 0); 
 let AverageSales = Sum_sales / salesfigures.length;
+
 console.log(`Average Sales = ${AverageSales}`)
+ 
+return AverageSales; 
 
 }
 
-let salesfigures = [120,150,130];
+let salesfigures = [5000,8000,10000];
 CalculateAverageSales(salesfigures);
 
+// Create a function to Determine Performance Rating 
 
+function determinePerformanceRating(AverageSales){
+
+    if (AverageSales > 10000) {
+        console.log(`Excellent`)
+
+    } else if ( AverageSales <= 10000 && AverageSales >= 7000) {
+        console.log(`Good`)
+    
+    } else if (AverageSales < 7000 && AverageSales > 4000) {
+    console.log(`Satisfactory`)
+
+    } else {
+        console.log(`Needs Improvement`)
+    }
+}
+
+determinePerformanceRating(AverageSales);
